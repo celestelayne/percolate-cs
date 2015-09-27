1b24b4d7d2ea863ba5bd273c9d4d9830e0f87c74
Celeste Layne <layne.celeste@gmail.com>
2015-09-26 20:14:40 -0700

    updated process and index files

--- process.php
+++ process.php
@@ -1,37 +1,52 @@
 <?php
-    
-$message = '';
 
-// Check that form fields are not empty
-    if (empty($_POST['FirstName']) || empty($_POST['LastName']) || empty($_POST['Phone']) || empty($_POST['Email']) || empty($_POST['Company']) ){
-        die('Name is required.');
+    if (isset($_POST["submit"])) {
+        $firstname = $_POST['FirstName'];
+        $lastname = $_POST['LastName'];
+        $phone = $_POST['Phone'];
+        $email = $_POST['Email'];
+        $company = $_POST['Company'];
+        $jobfunction = $_POST['Job_Function'];
+        $from = $_REQUEST['Email'];
+        $to = 'layne.celeste@gmail.com'; // substitute out your email address to receive submit data
+        $header = "From:" . $from . "\r\n";
+        $header .= "cc:" . $to . "\r\n";
+        $subject = 'Message from Shinola Case Study Page';
+
+        $body = $firstname . " " . "Thanks for requesting a demo! We’ll get back to you soon.";
     }
 
-    if($_POST['Job_Function'] == '-'){
-        die('Please select a job function.');
+    // Check if first name entered
+    if (empty($_POST['FirstName'])) {
+        $errFName = 'Please enter first name';
     }
-
-    // Construct the emaail message
-$message .= <<<TEXT
-    First_Name: {$_POST['FirstName']}
-    Last_Name: {$_POST['LastName']}
-    Phone: {$_POST['Phone']}
-    Email: {$_POST['Email']}
-    Company: {$_POST['Company']}
-    Job_Function: {$_POST['Job_Function']}
-TEXT;
-
-$to = 'layne.celeste@gmail.com';
-$from = $_REQUEST['Email'];
-$subject = 'You have been contacted';
-$header = 'From: ' . $from;
-
-$body = 'Thanks for requesting a demo! We’ll get back to you soon.';
-
-if(!mail($to, $subject, $message, $header, $body)){
-    print("<p class='Error'>Problem in Sending Mail.</p>");
-} else {
-    die( "<p class='success'>Mail Sent.</p>");
+    // Check if last name entered
+    if (empty($_POST['LastName'])) {
+        $errLName = 'Please enter last name';
+    }
+    // Check if phone number entered
+    if (empty($_POST['Phone'])) {
+        $errPhone = 'Please enter a valid phone number';
+    }
+    // Check if email address entered and is valid
+    if (empty($_POST['Email'] || !filter_var($_POST['Email'], FILTER_VALIDATE_EMAIL))) {
+        $errEmail = 'Please enter a valid email address'
+    }
+    // Check if company name entered
+    if (empty($_POST['Company'])) {
+        $errCompany = 'Please enter company name';
+    }
+    // Check if job function entered
+    if(!$_POST['Job_Function']) {
+        $errJFunc = 'Please select from the dropdown';
+    }
+// if there are no errors, send the email
+if(!$errFName && !$errLName && !$errPhone && !$errEmail && !$errCompany && !$errJFunc) {
+    if(mail($to, $subject, $header, $body, $from)) {
+        $result='<div class="alert alert-success">Thank You! We will be in touch</div>';
+    } else {
+        $result='<div class="alert alert-danger">Sorry there was an error sending your message.</div>';
+    }
 }
 
 ?>
